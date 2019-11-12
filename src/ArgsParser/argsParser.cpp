829abe8a67e736e8bcb7f36415efc9c4aec168e6
Antoine Cordelle <antoinecordelle@gmail.com>
2019-11-12 20:39:35 +0100

    Update help parameter

--- src/ArgsParser/argsParser.cpp
+++ src/ArgsParser/argsParser.cpp
@@ -7,7 +7,12 @@ using namespace std;
 bool ArgsParser::parseArgs(Arguments &args, int argc, char **argv) {
     for (int i = 1; i < argc; ++i) {
         if (string(argv[i]) == "-h" || string(argv[i]) == "--help") {
-            cout << "Help" << endl;
+            cout << "Http log parser. This program will display statistics on a 10sec time frame and alerting"
+                 << "in case of incoming traffic (2min time frame)." << endl;
+            cout << "Parameters : " << endl;
+            cout << "  -f / --filePath : path of the csv file to be analysed" << endl;
+            cout << "  -t / --threshold : integer to determine the alert threshold : number of request per second "
+                 << "that will trigger an alert" << endl;
             return false;
         }
         if (string(argv[i]) == "-f" || string(argv[i]) == "--filePath") {
