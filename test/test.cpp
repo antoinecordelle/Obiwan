94ec7d53291c0e6a4046e936a8ed570453b42ccf
Antoine Cordelle <antoinecordelle@gmail.com>
2019-11-13 13:19:38 +0100

    Set up test env

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+// Test Alert Logic
+TEST ( TestAlertLogic, AlertLogic1)
+{
+
+}
+
+int main(int argc,char *argv[]) {
+    ::testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
